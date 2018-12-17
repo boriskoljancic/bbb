5943f0bd83bee2585dbbc8832153119790d8f488
Boris <koljancicb@gmail.com>
2018-12-17 22:43:09 +0100

    Initial commit by mjotanovic

---
+++ run.bat
@@ -0,0 +1,12 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::      Dev environment startup script for Alfresco Community     ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@ECHO OFF
+
+IF "%MAVEN_OPTS%" == "" (
+    ECHO The environment variable 'MAVEN_OPTS' is not set, setting it for you
+    SET MAVEN_OPTS=-Xmx1024M -XX:MaxPermSize=512m
+)
+ECHO MAVEN_OPTS is set to '%MAVEN_OPTS%'
+
+mvn clean install alfresco:run
